6042bdd92204f49732a16ad67a90864edc027b7b
NexyusNex <shootdietale@gmail.com>
2022-10-21 15:06:33 +0200

    Reverse string exercise is solved and all tests have passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+let string = 'boy';
+const reverseString = function(string) {
+    let result = '';
+    for(let i = string.length;i>0;i--){
+        result = result + string.charAt(string.length-1);
+        string = string.slice(0,-1);
+    }
+    return result;
 };
 
 // Do not edit below this line
