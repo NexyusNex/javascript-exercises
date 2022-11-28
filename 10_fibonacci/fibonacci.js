b1b6d7affff36ca94c6a5a72c48808a9ba87523c
NexyusNex <shootdietale@gmail.com>
2022-11-28 16:17:48 +0100

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<1) return 'OOPS';
+    if(num===0) return 0;
+    Number(num);
+    var temp1=0, temp2=1, sum=1;
+    for(var i=1;i<num;i++){
+        sum=temp1 + temp2;
+        temp1 = temp2;
+        temp2 = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
